6d5c862c3de3e8c9840f8851c477a00b11621ae8
Kizilov <kizilov.yura@gmail.com>
2017-12-30 01:21:48 +0300

    make desctop

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,8 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+	const inputs = document.querySelectorAll('[data-check="true"]');
+	for (let i = 0; i < inputs.length; i++) {
+		inputs[i].setAttribute('checked', 'checked');
+	}
 });
